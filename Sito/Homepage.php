dc09f9d3e1fb70f226b7c3d42e17a4b5fc7ef8de
ViTtR0 <123543077+ViTtR0@users.noreply.github.com>
2024-01-17 11:57:45 +0100

    Regex registrazione, homepage
    Fatto reg ex per email password e nickname in registrazione, e aggiunto scheletro homepage e css

---
+++ Sito/Homepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="homepagecss.css">
+    <title>Home page</title>
+    <?php
+    echo '<h1>Serra Games</h1>
+    <table class="hometable">
+    <tr>
+        <td>Seleziona Giochi</td>
+    </tr>
+    '
+    ?>
+    
+</head>
+<body>
+    
+</body>
+</html>
