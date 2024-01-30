a5bf953e2dfefb678db6ead1c04e4cbd73d4b254
ViTtR0 <123543077+ViTtR0@users.noreply.github.com>
2024-01-30 08:59:25 +0100

    update grafici

---
+++ Homepage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="homepagecss.css">
+    <title>Home page</title>
+    <?php
+    echo '<table class="table1"><tr><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td><h1>Serra Games</h1></td><td class="td1"></td><td class="td1"></td></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td><td class="td1"></td></tr></table>
+    <table class="tablehome">
+    <tr>
+        <td style="text-align: center;"><h1>Seleziona Giochi</h1></td>
+    </tr>
+    <tr class="tr1"></tr><tr class="tr1"></tr><tr class="tr1"></tr><tr class="tr1"></tr><tr class="tr1"></tr>
+    </table>
+    '
+    ?>
+    
+</head>
+<body>
+    
+</body>
+</html>
